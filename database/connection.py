f55748a2a36c9a6cf5c7d6d31d1df4547f938d68
RubinaKhanal7 <rubikhanal8@gmail.com>
2025-07-10 17:57:03 +0545

    made the system modular

---
+++ database/connection.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config.settings import settings
+
+engine = create_engine(settings.database_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    """Get database session - this is a dependency"""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def create_tables():
+    """Create all tables"""
+    Base.metadata.create_all(bind=engine)
